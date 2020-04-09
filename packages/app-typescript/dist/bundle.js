9e65c1737fd7ccaa0776c8fed4b58abde6fd32b1
Pete Otaqui <pete@otaqui.com>
2020-04-09 16:01:50 +0100

    Fixed angular package name, updated postinstall script

---
+++ packages/app-typescript/dist/bundle.js
@@ -0,0 +1,6 @@
+define("index", ["require", "exports", "@ecma-modules/lib-date"], function (require, exports, lib_date_1) {
+    "use strict";
+    exports.__esModule = true;
+    console.log(lib_date_1.getFirstFormattedDate());
+});
+//# sourceMappingURL=bundle.js.map
