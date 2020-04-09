6838e76762574abf859043e4e0f24403c52f547a
Pete Otaqui <pete@otaqui.com>
2020-04-09 16:02:58 +0100

    Removed some cruft from typescript

--- packages/app-typescript/dist/bundle.js
+++
@@ -1,6 +0,0 @@
-define("index", ["require", "exports", "@ecma-modules/lib-date"], function (require, exports, lib_date_1) {
-    "use strict";
-    exports.__esModule = true;
-    console.log(lib_date_1.getFirstFormattedDate());
-});
-//# sourceMappingURL=bundle.js.map
